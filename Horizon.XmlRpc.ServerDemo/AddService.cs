7d5e392e3440aa6d410b5af0f68d6d3d02bf76ee
Stefan Wilkes <stefan.wilkes@gmail.com>
2019-01-27 18:51:14 +0100

    Added demo for XML-RPC Server

---
+++ Horizon.XmlRpc.ServerDemo/AddService.cs
@@ -0,0 +1,13 @@
+﻿using CookComputing.XmlRpc;
+
+namespace Horizon.XmlRpc.ServerDemo
+{
+    internal class AddService : XmlRpcListenerService
+    {
+        [XmlRpcMethod("Demo.addNumbers", Description = "Return product of number a and number b")]
+        public int AddNumbers(int numberA, int numberB)
+        {
+            return numberA + numberB;
+        }
+    }
+}
---
+++ Horizon.XmlRpc.ServerDemo/Program.cs
@@ -0,0 +1,21 @@
+﻿using System.Net;
+
+namespace Horizon.XmlRpc.ServerDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var listener = new HttpListener();
+            listener.Prefixes.Add("http://127.0.0.1:5678/");
+            listener.Start();
+
+            while (true)
+            {
+                var context = listener.GetContext();
+                var service = new AddService();
+                service.ProcessRequest(context);
+            }
+        }
+    }
+}
