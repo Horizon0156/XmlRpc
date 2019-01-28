b4db804c13789ad318382c49c9038581312f4cd5
Stefan Wilkes <stefan.wilkes@gmail.com>
2019-01-28 18:41:29 +0100

    Added another demo for communication with a Homematic CCU

---
+++ CcuDemo/IHomeMaticProxy.cs
@@ -0,0 +1,16 @@
+﻿using CookComputing.XmlRpc;
+
+namespace CcuDemo
+{
+    public interface IHomeMaticProxy : IXmlRpcProxy
+    {
+        [XmlRpcMethod("listDevices")]
+        DeviceDescription[] ListDevices();
+
+        [XmlRpcMethod("setValue")]
+        void SetValue(string address, string valueKey, object value);
+
+        [XmlRpcMethod("getValue")]
+        object GetValue(string address, string valueKey);
+    }
+}
---
+++ CcuDemo/Program.cs
@@ -0,0 +1,29 @@
+﻿using CookComputing.XmlRpc;
+using System;
+
+namespace CcuDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Collection devices from CCU...");
+            var proxy = XmlRpcProxyGen.Create<IHomeMaticProxy>();
+            proxy.Url = "http://192.168.178.49:2010";
+
+            var result = proxy.ListDevices();
+
+            foreach (var device in result)
+            {
+                Console.WriteLine(device);
+            }
+
+            Console.WriteLine("Turning Retro-Licht on...");
+            //proxy.SetValue("000218A9928E62:3", "STATE", true); // Working but annoying ;)
+
+
+            Console.WriteLine(Environment.NewLine + "Press any key to exit...");
+            Console.ReadKey();
+        }
+    }
+}
